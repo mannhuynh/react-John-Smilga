52d4332988fe229beae2af9267356e14c2b1fb3a
Vuong Huynh <mannhuynh@gmail.com>
2021-08-19 13:55:43 -0500

    Add Ternery and Short Circuit

--- react-advanced-2020-master/src/App.js
+++ react-advanced-2020-master/src/App.js
@@ -6,13 +6,13 @@ import UseState5 from "./tutorial/1-useState/setup/5-useState-counter";
 import UseEffect1 from "./tutorial/2-useEffect/setup/1-useEffect-basics";
 import UseEffect2 from "./tutorial/2-useEffect/setup/2-useEffect-cleanup";
 import UseEffect3 from "./tutorial/2-useEffect/setup/3-useEffect-fetch-data";
+import Conditional1 from "./tutorial/3-conditional-rendering/setup/1-multiple-returns";
+import Conditional2 from "./tutorial/3-conditional-rendering/setup/2-short-circuit";
 
 function App() {
 	return (
 		<div className="container">
-			<h1 style={{ paddingBottom: "0px", marginBottom: "0px" }}>
-				Advanced Tutorial
-			</h1>
+			<h1>Advanced Tutorial</h1>
 			<hr />
 			<UseState1 />
 			<UseState3 />
@@ -21,6 +21,8 @@ function App() {
 			<UseEffect1 />
 			<UseEffect2 />
 			<UseEffect3 />
+			<Conditional1 />
+			<Conditional2 />
 		</div>
 	);
 }
--- react-advanced-2020-master/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ react-advanced-2020-master/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+	const [text, setText] = useState("");
+	const [isError, setIsError] = useState(false);
 
-  return <h2>short circuit</h2>;
+	const firstValue = text || "hello world";
+	const secondValue = text && "hello world";
+
+	return (
+		<>
+			<h2>short circuit</h2>
+			<h3>First value: {firstValue}</h3>
+			<h3>Second value: {secondValue}</h3>
+
+			<h4>OR '||' : {text || "TIM"}</h4>
+			<h4>AND '&&' : {text && "TIM"}</h4>
+			<h4>NOT '!' : {!text && "TIM"}</h4>
+			<button className="btn" onClick={() => setIsError(!isError)}>
+				Toggle Error
+			</button>
+			<h3>Display: {isError && "ERROR..."}</h3>
+			{isError ? (
+				<p>There is an error... </p>
+			) : (
+				<div>
+					<h3>There is no error</h3>
+				</div>
+			)}
+		</>
+	);
 };
 
 export default ShortCircuit;
