c9a48529b7ff9c44ad3f5be98292665641401c31
Vuong Huynh <mannhuynh@gmail.com>
2021-08-24 14:22:26 -0500

    Modify Birthday List App

--- react-projects-master/01-birthday-reminder/setup/src/App.js
+++ react-projects-master/01-birthday-reminder/setup/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import data from "./data";
-import List from "./List";
+// import List from "./List";
 
 function App() {
 	const [people, setPeople] = useState(data);
@@ -9,7 +9,19 @@ function App() {
 		<main>
 			<section className="container">
 				<h3>{people.length} Birthdays Today</h3>
-				<List people={people} />
+				{/* <List people={people} /> */}
+				{people.map((person) => {
+					const { id, name, age, image } = person;
+					return (
+						<article key={id} className="person">
+							<img src={image} alt={name} />
+							<div>
+								<h4>{name}</h4>
+								<p>{age} years</p>
+							</div>
+						</article>
+					);
+				})}
 				<button onClick={() => setPeople([])}>Clear All</button>
 			</section>
 		</main>
