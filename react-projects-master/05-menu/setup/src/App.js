9c9035eb65db43488eafdd8b0eb3deb4a761f470
Vuong Huynh <mannhuynh@gmail.com>
2021-08-25 14:56:27 -0500

    add Menu App

--- react-projects-master/05-menu/setup/src/App.js
+++ react-projects-master/05-menu/setup/src/App.js
@@ -1,10 +1,37 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
+import menu from "./data";
+
+const itemCategories = new Set(items.map((item) => item.category));
+const allCategories = ["all", ...itemCategories];
+// console.log(allCategories);
 
 function App() {
-  return <h2>menu project setup</h2>;
+	const [menuItems, setMenuItems] = useState(items);
+	const [categories, setCategories] = useState(allCategories);
+
+	const filterItems = (category) => {
+		if (category === "all") {
+			setMenuItems(items);
+			return;
+		}
+		const newItems = items.filter((item) => item.category === category);
+		setMenuItems(newItems);
+	};
+	return (
+		<main>
+			<section className="menu section">
+				<div className="title">
+					<h2>Our Menu</h2>
+					<div className="underline"></div>
+				</div>
+				<Categories categories={categories} filterItems={filterItems} />
+				<Menu items={menuItems} />
+			</section>
+		</main>
+	);
 }
 
 export default App;
