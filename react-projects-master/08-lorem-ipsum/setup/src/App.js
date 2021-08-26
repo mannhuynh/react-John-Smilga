309d74481d4cac973ed949d35c7c07447f5e8fe9
Vuong Huynh <mannhuynh@gmail.com>
2021-08-26 18:33:20 -0500

    Add Lorem Ipsum Generator

--- react-projects-master/08-lorem-ipsum/setup/src/App.js
+++ react-projects-master/08-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,44 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 function App() {
-  
-  
-  return (
-  <h2>lorem ipsum project setup</h2>
-    )
+	const [count, setCount] = useState(0);
+	const [text, setText] = useState([]);
+	// const textLength
+	// console.log(textLength);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		let amount = parseInt(count);
+		if (amount >= 8) {
+			amount = 8;
+		} else if (amount <= 0) {
+			amount = 1;
+		}
+		setText(data.slice(0, amount));
+	};
+
+	return (
+		<section className="section-center">
+			<h2>Tired of boring Lorem Ipsum???</h2>
+			<form action="" className="lorem-form" onClick={handleSubmit}>
+				<label htmlFor="amout">Paragraphs:</label>
+				<input
+					type="number"
+					name="amount"
+					value={count}
+					id="amount"
+					onChange={(e) => setCount(e.target.value)}
+				/>
+				<button className="btn" type="submit">
+					Generate
+				</button>
+			</form>
+
+			{text.map((item, index) => {
+				return <p key={index}>{item}</p>;
+			})}
+		</section>
+	);
 }
 
 export default App;
