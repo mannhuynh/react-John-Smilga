8f3100dbb149e35514489410dd1d77be40dbeb76
Vuong Huynh <mannhuynh@gmail.com>
2021-08-27 17:12:59 -0500

    Add Grocery App

--- react-projects-master/10-grocery-bud/setup/src/App.js
+++ react-projects-master/10-grocery-bud/setup/src/App.js
@@ -1,9 +1,108 @@
-import React, { useState, useEffect } from 'react'
-import List from './List'
-import Alert from './Alert'
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
+
+const getLocalStorage = () => {
+	let list = localStorage.getItem("list");
+	if (list) {
+		return JSON.parse(localStorage.getItem("list"));
+	} else {
+		return [];
+	}
+};
 
 function App() {
-  return <h2>grocery bud setup</h2>
+	const [name, setName] = useState("");
+	const [list, setList] = useState(getLocalStorage());
+	const [isEditing, setIsEditing] = useState(false);
+	const [editID, setEditID] = useState(null);
+	const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		if (!name) {
+			// display Alert
+			showAlert(true, "danger", "Please Enter Item");
+		} else if (name && isEditing) {
+			// editing
+			setList(
+				list.map((item) => {
+					if (item.id == editID) {
+						return { ...item, title: name };
+					}
+					return item;
+				})
+			);
+			setName("");
+			setEditID(null);
+			setIsEditing(false);
+			showAlert(true, "success", "Edit Successful!!!");
+		} else {
+			//more alert
+			showAlert(true, "success", "Item Added to the List");
+			const newItem = { id: new Date().getTime().toString(), title: name };
+			setList([...list, newItem]);
+			setName("");
+		}
+	};
+
+	const showAlert = (show = false, type = "", msg = "") => {
+		setAlert({ show, type, msg });
+	};
+
+	const clearList = () => {
+		showAlert(true, "danger", "List is Now Empty");
+		setList([]);
+	};
+
+	const removeItem = (id) => {
+		showAlert(true, "danger", "Item Removed");
+		setList(list.filter((item) => item.id !== id));
+	};
+
+	const editItem = (id) => {
+		const specificItem = list.find((item) => item.id === id);
+		setIsEditing(true);
+		setEditID(id);
+		setName(specificItem.title);
+	};
+
+	useEffect(() => {
+		localStorage.setItem("list", JSON.stringify(list));
+	}, [list]);
+
+	return (
+		<section className="section-center">
+			<form className="grocery-form" onSubmit={handleSubmit}>
+				{alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
+
+				<h3>Grocery List</h3>
+				<div className="form-control">
+					<input
+						type="text"
+						className="grocery"
+						placeholder="items"
+						value={name}
+						onChange={(e) => setName(e.target.value)}
+					/>
+					<button className="submit-btn">
+						{isEditing ? "Edit" : "Submit"}
+					</button>
+				</div>
+			</form>
+
+			{list.length > 0 && (
+				<div className="grocery-container">
+					<List items={list} removeItem={removeItem} editItem={editItem} />
+
+					<button className="clear-btn" onClick={clearList}>
+						Clear Items
+					</button>
+				</div>
+			)}
+		</section>
+	);
 }
 
-export default App
+export default App;
--- react-projects-master/10-grocery-bud/setup/src/List.js
+++ react-projects-master/10-grocery-bud/setup/src/List.js
@@ -1,7 +1,27 @@
-import React from 'react'
-import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = () => {
-  return <h2>list component</h2>
-}
+import React from "react";
+import { FaEdit, FaTrash } from "react-icons/fa";
 
-export default List
+const List = ({ items, removeItem, editItem }) => {
+	return (
+		<div className="groery-list">
+			{items.map((item) => {
+				const { id, title } = item;
+				return (
+					<article key={id} className="grocery-item">
+						<p className="title">{title}</p>
+						<div className="but-container">
+							<button className="edit-btn" onClick={() => editItem(id)}>
+								<FaEdit />
+							</button>
+							<button className="delete-btn" onClick={() => removeItem(id)}>
+								<FaTrash />
+							</button>
+						</div>
+					</article>
+				);
+			})}
+		</div>
+	);
+};
+
+export default List;
